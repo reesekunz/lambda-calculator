fa0f479f273e62fa977a12b9d38cf29ff212e1d2
Reese Kunz <reesekunz21@gmail.com>
2019-07-16 13:50:38 -0700

    added exports to data.js and imports to numbers.js, finishing imports

--- src/components/ButtonComponents/NumberButtons/Numbers.js
+++ src/components/ButtonComponents/NumberButtons/Numbers.js
@@ -1,11 +1,18 @@
-import React from "react";
+
+import React, { useState } from "react";
 
 //import any components needed
 
+import NumberButton from "./NumberButton."
+
 //Import your array data to from the provided data file
+
+import { numbers } from '../../../data'
 
 const Numbers = () => {
   // STEP 2 - add the imported data to state
+
+ 
   return (
     <div>
       {/* STEP 3 - Use .map() to iterate over your array data and return a button
--- src/data.js
+++ src/data.js
@@ -4,9 +4,12 @@
 // file. No real tricks here just be aware of what is in each array
 // and how you'll access the data.
 
-const numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "."];
+// add export in front of each array being imported into 
+// Numbers.js, Specials.js and Operators.js files (these are the wrapper components)
 
-const operators = [
+export const numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "."];
+
+export const operators = [
   {
     char: "/",
     value: "/"
@@ -29,4 +32,4 @@ const operators = [
   }
 ];
 
-const specials = ["C", "+/-", "%"];
+export const specials = ["C", "+/-", "%"];
